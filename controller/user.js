f0734bcfaebede9e4a3d97d603a36afe2f23f1f2
KhizarAhmed000 <khizar13ahmed@gmail.com>
2024-03-24 00:50:37 +0500

    .

--- controller/user.js
+++ controller/user.js
@@ -3,26 +3,33 @@ const User = require("../models/user");
 require("dotenv").config();
 
 exports.createUser = async (req, res, next) => {
-    try {
-      const { walletAddress, transactions } = req.body;
-  
-      const existingUser = await User.findOne({ walletAddress });
-  
-      if (existingUser) {
-        return res.status(400).json({ status: 400, message: "User already exists" });
-      } else {
-        const newUser = await User.create({
-          walletAddress,
-          transactions,
-        });
-  
-        return res.status(201).json({ message: "User created", newUser });
-      }
-    } catch (error) {
-      console.error("Error creating user:", error);
-      return res.status(500).json({ message: "Internal Server Error" });
+  try {
+    const { walletAddress, transactions } = req.body;
+
+    const existingUser = await User.findOne({ walletAddress });
+
+    if (existingUser) {
+      const existingTransactions = existingUser.transactions || [];
+      const updatedTransactions = [...existingTransactions, ...transactions];
+
+      // Update existing user with the new transactions
+      await User.findOneAndUpdate({ walletAddress }, { transactions: updatedTransactions });
+
+      return res.status(200).json({ message: "Transactions appended to existing user", updatedTransactions });
+    } else {
+      // User doesn't exist, create a new user
+      const newUser = await User.create({
+        walletAddress,
+        transactions,
+      });
+
+      return res.status(201).json({ message: "User created", newUser });
     }
-  };
+  } catch (error) {
+    console.error("Error creating or updating user:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 
   exports.getUser = async (req, res, next) => {
